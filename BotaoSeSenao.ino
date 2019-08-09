9afe69fa3ee9b51b636b42f6bb2446af4e2f350d
ArduInoSketch <wermesonfernandes@gmail.com>
2019-08-09 09:46:40 -0300

    update BotaoSeSenao.ino

--- BotaoSeSenao.ino
+++ BotaoSeSenao.ino
@@ -1,21 +1,30 @@
-#define botamRed 6
-#define botamYellow 7
-#define ledRed 8
-#define ledYellow 9
-
+#define led1 6 //DEFINE PINOS
+#define led2 7
+#define botao1 8
+#define botao2 9
 void setup() {
-  pinMode(botamRed, INPUT);
-  pinMode(botamYellow, INPUT);
-  pinMode(ledRed, OUTPUT);
-  pinMode(ledYellow, OUTPUT);
+  pinMode(led1, OUTPUT); //SAIDA
+  pinMode(led2, OUTPUT);
+  pinMode(botao1, INPUT_PULLUP); //ENTRADA
+  pinMode(botao2, INPUT_PULLUP);
 }
-
 void loop() {
-  //SE ISTO
-  if ((digitalRead(botamRed) == HIGH)) {
-    //OPERADOR ! INVERTE O ESTADO DA LED
-    digitalWrite(ledRed, !digitalRead(ledRed));
-  } else if ((digitalRead(botamYellow) == HIGH)) { //SE NAO ISTO
-    digitalWrite(ledYellow, !digitalRead(ledYellow));
+  //SE FOR VERDADEIRO LIGA A LED1 E DESLIGA LED2
+  if ((digitalRead(botao1) == LOW) && (digitalRead(botao2) == HIGH)) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    //ENTÃO SE FOR VERDADEIRO LIGA A LED2 E DESLIGA LED1
+  } else if ((digitalRead(botao2) == LOW) && (digitalRead(botao1) == HIGH)) {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, HIGH);
+    //ENTÃO SE FOR VERDADEIRO DESLIGA AS LEDS
+  } else if ((digitalRead(botao2) == LOW) && (digitalRead(botao1) == LOW)) {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    //ENTÃO SE NENHUMA FOR VERDADEIRA LIGA TODAS LEDS
+  } else {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
   }
+  delay(200);
 }
